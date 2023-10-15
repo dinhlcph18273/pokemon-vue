eea67cdc6881a32d5af7b06b6a83aac0aafc7aaf
dinhlc <dinhlc@vnext.vn>
2023-10-15 23:25:37 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <main-screen
+    v-if="statusMatch === 'default'"
+    @onStart="onHandleBeforStart($event)"
+  />
+  <interact-screen
+    v-if="statusMatch === 'match'"
+    :cardsContext="settings.cardsContext"
+    @onFinish="onGetResult()"
+  />
+  <result-screen
+    v-if="statusMatch === 'result'"
+    :timer="timer"
+    @startAgain="statusMatch = 'default'"
+  />
+  <coppy-right />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import MainScreen from "./components/MainScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+import ResultScreen from "./components/ResultScreen.vue";
+import { shuffed } from "./untils/array.js";
+import CoppyRight from "./components/CoppyRightScreen.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    MainScreen,
+    InteractScreen,
+    ResultScreen,
+    CoppyRight,
+  },
+  data() {
+    return {
+      settings: {
+        totalOfBlock: 0,
+        cardsContext: [],
+        startedAt: null,
+        timer: 0,
+      },
+      statusMatch: "default",
+    };
+  },
+  methods: {
+    onHandleBeforStart(config) {
+      this.settings.totalOfBlock = config.totalOfBlock;
+
+      const firstCards = Array.from(
+        { length: this.settings.totalOfBlock / 2 },
+        (_, i) => i + 1
+      );
+      const secondCards = [...firstCards];
+      const card = [...firstCards, ...secondCards];
+      this.settings.cardsContext = shuffed(shuffed(shuffed(shuffed(card))));
+      this.settings.startedAt = new Date().getTime();
+
+      this.statusMatch = "match";
+    },
+    onGetResult() {
+      this.timer = new Date().getTime() - this.settings.startedAt;
+
+      console.log(this.timer);
+      this.statusMatch = "result";
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
